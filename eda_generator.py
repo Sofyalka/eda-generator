098b0dbdd984e47cf973ea76a502236f8e56b73a
Sofyalka <117361271+Sofyalka@users.noreply.github.com>
2025-05-21 15:09:30 +0300

    Update eda_generator.py

--- eda_generator.py
+++ eda_generator.py
@@ -3,25 +3,7 @@
 
 # In[5]:
 
-
 #pip install ydata-profiling
-
-
-# In[10]:
-
-
-#import pandas as pd
-#from ydata_profiling import ProfileReport
-#df = pd.read_csv('dataset_olympics.csv')
-#profile = ProfileReport(df)
-#profile.to_file("report.html")
-
-
-# In[1]:
-
-
-#pip install weasyprint
-
 
 # In[5]:
 
@@ -32,82 +14,5 @@ from ydata_profiling import ProfileReport
 def generate_eda_report(df, output_path):
     profile = ProfileReport(df, title = "Автоматический EDA-отчет", explorative=True)
     profile.to_file(output_path)
-
-
-# In[9]:
-
-
-#pip install "fastapi[standard]"
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
